545efde27d2f95ce13e9d0ce951d7e653c35c5fc
Martin Nguyen <martinnguyen0523@gmail.com>
2019-06-04 18:55:04 -0700

    refactored handle_movement() to be more maintainable

--- src/main.c
+++ src/main.c
@@ -2307,7 +2307,7 @@ void on_char(GLFWwindow *window, unsigned int u) {
     }
 }
 
-void on_scroll(double ydelta) {
+void on_scroll(GLFWwindow *window, double xdelta, double ydelta) {
     static double ypos = 0;
     ypos += ydelta;
     if (ypos < -SCROLL_THRESHOLD) {
@@ -2408,64 +2408,102 @@ void handle_mouse_input() {
     }
 }
 
+void handle_ortho() {
+	if (!g->typing) {
+		g->ortho = glfwGetKey(g->window, CRAFT_KEY_ORTHO) ? 64 : 0;
+	}
+}
+
+void handle_fov() {
+	if (!g->typing) {
+		g->fov = glfwGetKey(g->window, CRAFT_KEY_ZOOM) ? 15 : 65;
+	}
+}
+void get_wasd_input(int *sz, int *sx, State *s, int m) {
+	if (!g->typing) {
+		if (glfwGetKey(g->window, CRAFT_KEY_FORWARD)) *sz--;
+		if (glfwGetKey(g->window, CRAFT_KEY_BACKWARD)) *sz++;
+		if (glfwGetKey(g->window, CRAFT_KEY_LEFT)) *sx--;
+		if (glfwGetKey(g->window, CRAFT_KEY_RIGHT)) *sx++;
+		if (glfwGetKey(g->window, GLFW_KEY_LEFT)) s->rx -= m;
+		if (glfwGetKey(g->window, GLFW_KEY_RIGHT)) s->rx += m;
+		if (glfwGetKey(g->window, GLFW_KEY_UP)) s->ry += m;
+		if (glfwGetKey(g->window, GLFW_KEY_DOWN)) s->ry -= m;
+	}
+}
+
+void get_jump_input(float *vy, float *dy) {
+	if (!g->typing) {
+		if (glfwGetKey(g->window, CRAFT_KEY_JUMP)) {
+			if (g->flying) {
+				*vy = 1;
+			}
+			else if (dy == 0) {
+				*dy = 8;
+			}
+		}
+	}
+}
+
+int estimate_steps(float vx, float vy, float vz, float speed, float dy, double dt) {
+	int estimate = roundf(sqrtf(
+		powf(vx * speed, 2) +
+		powf(vy * speed + ABS(dy) * 2, 2) +
+		powf(vz * speed, 2)) * dt * 8);
+	return estimate;
+}
+
+float handle_gravity(float dy, float ut) {
+	if (g->flying) {
+		dy = 0;
+	}
+	else {
+		dy -= ut * 25;
+		dy = MAX(dy, -250);
+	}
+}
+
+void handle_player_under_world(State *s) {
+	if (s->y < 0) {
+		s->y = highest_block(s->x, s->z) + 2;
+	}
+}
+
 void handle_movement(double dt) {
-    static float dy = 0;
-    State *s = &g->players->state;
-    int sz = 0;
-    int sx = 0;
-    if (!g->typing) {
-        float m = dt * 1.0;
-        g->ortho = glfwGetKey(g->window, CRAFT_KEY_ORTHO) ? 64 : 0;
-        g->fov = glfwGetKey(g->window, CRAFT_KEY_ZOOM) ? 15 : 65;
-        if (glfwGetKey(g->window, CRAFT_KEY_FORWARD)) sz--;
-        if (glfwGetKey(g->window, CRAFT_KEY_BACKWARD)) sz++;
-        if (glfwGetKey(g->window, CRAFT_KEY_LEFT)) sx--;
-        if (glfwGetKey(g->window, CRAFT_KEY_RIGHT)) sx++;
-        if (glfwGetKey(g->window, GLFW_KEY_LEFT)) s->rx -= m;
-        if (glfwGetKey(g->window, GLFW_KEY_RIGHT)) s->rx += m;
-        if (glfwGetKey(g->window, GLFW_KEY_UP)) s->ry += m;
-        if (glfwGetKey(g->window, GLFW_KEY_DOWN)) s->ry -= m;
-    }
-    float vx, vy, vz;
-    get_motion_vector(g->flying, sz, sx, s->rx, s->ry, &vx, &vy, &vz);
-    if (!g->typing) {
-        if (glfwGetKey(g->window, CRAFT_KEY_JUMP)) {
-            if (g->flying) {
-                vy = 1;
-            }
-            else if (dy == 0) {
-                dy = 8;
-            }
-        }
-    }
-    float speed = g->flying ? 20 : 5;
-    int estimate = roundf(sqrtf(
-        powf(vx * speed, 2) +
-        powf(vy * speed + ABS(dy) * 2, 2) +
-        powf(vz * speed, 2)) * dt * 8);
-    int step = MAX(8, estimate);
-    float ut = dt / step;
-    vx = vx * ut * speed;
-    vy = vy * ut * speed;
-    vz = vz * ut * speed;
-    for (int i = 0; i < step; i++) {
-        if (g->flying) {
-            dy = 0;
-        }
-        else {
-            dy -= ut * 25;
-            dy = MAX(dy, -250);
-        }
-        s->x += vx;
-        s->y += vy + dy * ut;
-        s->z += vz;
-        if (collide(2, &s->x, &s->y, &s->z)) {
-            dy = 0;
-        }
-    }
-    if (s->y < 0) {
-        s->y = highest_block(s->x, s->z) + 2;
-    }
+	static float dy = 0;
+	State *s = &g->players->state;
+	int sz = 0;
+	int sx = 0;
+	float m = dt * 1.0;
+
+	handle_ortho();
+	handle_fov();
+	get_wasd_input(&sz, &sx, s, m);
+
+	float vx, vy, vz;
+	get_motion_vector(g->flying, sz, sx, s->rx, s->ry, &vx, &vy, &vz);
+
+	get_jump_input(&vy, &dy);
+
+	float speed = g->flying ? 20 : 5;
+	int estimate = estimate_steps(vx, vy, vz, speed, dy, dt);
+	int step = MAX(8, estimate);
+	float ut = dt / step;
+	vx = vx * ut * speed;
+	vy = vy * ut * speed;
+	vz = vz * ut * speed;
+
+	for (int i = 0; i < step; i++) {
+		dy = handle_gravity(dy, ut);
+		s->x += vx;
+		s->y += vy + dy * ut;
+		s->z += vz;
+
+		if (collide(2, &s->x, &s->y, &s->z)) {
+			dy = 0;
+		}
+	}
+	handle_player_under_world(s);
 }
 
 void parse_buffer(char *buffer) {
